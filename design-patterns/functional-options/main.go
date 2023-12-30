0c18a432fc5e800da2ef693d4b30b03d5095e36d
v01 <vishnu.pradeep@yahooinc.com>
2023-12-30 17:35:26 +0530

    feat: added functional options

---
+++ design-patterns/functional-options/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+type OptFunc func(*Opts)
+
+type Opts struct {
+	maxConn int
+	id      string
+	tls     bool
+}
+
+func defaultOpts() Opts {
+	return Opts{
+		maxConn: 10,
+		id:      "default",
+		tls:     false,
+	}
+}
+
+func SetMaxConn(i int) OptFunc {
+	return func(o *Opts) {
+		o.maxConn = i
+	}
+}
+
+func EnableTLS(o *Opts) {
+	o.tls = true
+}
+
+type Server struct {
+	Opts
+}
+
+func NewServer(opts ...OptFunc) *Server {
+	o := defaultOpts()
+	for _, fn := range opts {
+		fn(&o)
+	}
+	return &Server{
+		o,
+	}
+}
+
+func main() {
+	server := NewServer(SetMaxConn(100), EnableTLS)
+	fmt.Println(server)
+}
--- design_patterns/functional_options/go.mod
+++
@@ -1,3 +0,0 @@
-module functional_options
-
-go 1.21
